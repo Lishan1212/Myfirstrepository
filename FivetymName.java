bea7c6b292471683c26dfdf14ccf8a0b2b294a45
Lishan1212 <lishalishan12gmail.com>
2021-11-18 13:50:43 +0530

    1st

---
+++ FivetymName.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class FivetymName
+{  
+
+public Static void main(String[]args)
+{
+  int i;
+String name;
+
+for(i=0;i<=5;i++)
+{
+System.out.println("lishan");
+
+}
+}
